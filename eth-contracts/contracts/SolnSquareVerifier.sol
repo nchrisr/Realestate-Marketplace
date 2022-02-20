e24986c5251cc08dc910c01623e018d68d205b09
Rodney <24297785+nchrisr@users.noreply.github.com>
2022-02-19 20:02:22 -0600

    Update SolnSquareVerifier.sol

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -3,10 +3,7 @@ pragma solidity >=0.4.21 <0.6.0;
 import "./ERC721Mintable.sol";
 import "./verifier.sol";
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 
-
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is ERC721MintableRealEstate{
 
     modifier solutionIsCorrect(
@@ -24,22 +21,19 @@ contract SolnSquareVerifier is ERC721MintableRealEstate{
         verifierContract = Verifier(verifierAddress);
     }
 
-    // TODO define a solutions struct that can hold an index & an address
     struct Solution {
         uint256 index;
         address to;
     }
 
-    // TODO define an array of the above struct
+
     Solution[] solutions;
 
-    // TODO define a mapping to store unique solutions submitted
+
     mapping(bytes32 => Solution) private uniqueSolutions;
 
-    // TODO Create an event to emit when a solution is added
     event SolutionAdded(address to, uint256 index);
 
-    // TODO Create a function to add the solutions to the array and emit the event
     function addSolution(address _to, uint256 _index, bytes32 _key) internal {
         Solution memory _solution = Solution({index : _index, to : _to});
         solutions.push(_solution);
@@ -47,9 +41,7 @@ contract SolnSquareVerifier is ERC721MintableRealEstate{
         emit SolutionAdded(_to, _index);
     }
 
-    // TODO Create a function to mint new NFT only after the solution has been verified
-    //  - make sure the solution is unique (has not been used before)
-    //  - make sure you handle metadata as well as tokenSupply
+
     function mintToken(
         address _to,
         uint256 _tokenId,
